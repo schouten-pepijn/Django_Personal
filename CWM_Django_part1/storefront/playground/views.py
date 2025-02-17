798b7dba88093c900107f287d10308aab3d3180a
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-17 12:32:37 +0100

    add debug toolbar

--- CWM_Django_part1/storefront/playground/views.py
+++ CWM_Django_part1/storefront/playground/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return render(request, 'hello.html')
+    return render(request, 'hello.html', {'name': 'John Doe'})
