8939b37c65504c1e9e16d77a725d163ef0a19c7b
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-18 00:12:13 +0100

    added models to apps

--- CWM_Django_part1/e_commerce_site/store/models.py
+++ CWM_Django_part1/e_commerce_site/store/models.py
@@ -1,3 +1,109 @@
 from django.db import models
 
 # Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='+',
+    )
+
+
+class Product(models.Model):
+    # sku = models.CharField(max_length=10, primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(
+        Collection,
+        on_delete=models.PROTECT
+    )
+    promotions = models.ManyToManyField(Promotion)
+
+
+class Customer(models.Model):
+
+    MEMBERSHIP_CHOICES = [
+        ('B', 'Bronze'),
+        ('S', 'Silver'),
+        ('G', 'Gold'),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1,
+        choices=MEMBERSHIP_CHOICES,
+        default=MEMBERSHIP_CHOICES[0][0]
+    )
+
+
+class Order(models.Model):
+
+    PAYMENT_CHOICES = [
+        ('P', 'Pending'),
+        ('C', 'Completed'),
+        ('F', 'Failed'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1,
+        choices=PAYMENT_CHOICES,
+        default=PAYMENT_CHOICES[0][0],
+    )
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.PROTECT
+    )
+    
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.PROTECT
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.PROTECT
+    )
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+    quantity = models.PositiveSmallIntegerField()
+
+
+class Adress(models.Model):
+    # one-to-one relationship
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+    )
