c233ebe3eff715f856b2aa13db169b182bdbdb8a
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-18 21:17:34 +0100

    added ORM exersizes

--- CWM_Django_part1/storefront_ORM_practise/playground/views.py
+++ CWM_Django_part1/storefront_ORM_practise/playground/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from store.models import Product
+from django.db.models import Q, F
+
+from store.models import Product, OrderItem, Order
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
     
-    for product in query_set:
-        print(product)
+    # Products: inventory < 10 OR unit_price < 20
+    # queryset = Product.objects.filter(
+    #     Q(inventory__lt=10) | Q(unit_price__lt=20)
+    # ).order_by('-unit_price')[10:30]
     
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # Products: inventory = price
+    # queryset = Product.objects.filter(inventory=F('unit_price'))
+    
+    # queryset = Product.objects.values('id', 'title')
+    
+    # specify fields
+    # querysetOrderItems = OrderItem.objects \
+    #     .values('product__id') \
+    #         .distinct()
+    # queryset = Product.objects \
+    #     .filter(id__in=querysetOrderItems) \
+    #         .order_by('id')
+    
+    # queryset = Order.objects \
+    #     .select_related('customer') \
+    #         .prefetch_related('orderitem_set__product') \
+    #             .order_by('-placed_at')[:5]
+
+    return render(request, 'hello.html', {'name': 'Mosh', 'orders': list(queryset)})
