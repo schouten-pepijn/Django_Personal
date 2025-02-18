43a6be62909cbe51bbac082b92b4704079698900
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-18 19:46:55 +0100

    orm

---
+++ CWM_Django_part1/storefront_ORM_dummy copy/playground/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from store.models import Product
+
+
+def say_hello(request):
+    query_set = Product.objects.all()
+    
+    for product in query_set:
+        print(product)
+    
+    return render(request, 'hello.html', {'name': 'Mosh'})
