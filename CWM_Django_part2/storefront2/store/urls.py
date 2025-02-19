758557cf7170c016280ca3d348c8955307e76ceb
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-19 22:51:54 +0100

    create serializers

--- CWM_Django_part2/storefront2/store/urls.py
+++ CWM_Django_part2/storefront2/store/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('products/', views.product_list)
+    path('products/', views.product_list),
+    path('products/legacy', views.product_list_legacy),
+    
+    path('products/<int:id>', views.product_detail),
+    path('products/legacy/<int:id>', views.product_detail_legacy),
 ]
