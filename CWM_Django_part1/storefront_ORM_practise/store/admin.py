873cb8b1911c904f229cac91a3a9a4f43595d76a
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-19 15:14:07 +0100

    add admin page

--- CWM_Django_part1/storefront_ORM_practise/store/admin.py
+++ CWM_Django_part1/storefront_ORM_practise/store/admin.py
@@ -1,5 +1,45 @@
 from django.contrib import admin
 from . import models
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection']
+    
+    
+    # computed fields
+    @admin.display(ordering='inventory')
+    def inventory_status(self, obj):
+        if obj.inventory < 10:
+            return 'Low'
+        return 'OK'
+    
+    
+    def collection_title(self, obj):
+        return obj.collection.title
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    
+    
+
 # Register your models here.
-admin.site.register(models.Collection)
+admin.site.register(models.Product, ProductAdmin)
+
+admin.site.register(models.Customer, CustomerAdmin)
--- CWM_Django_part1/storefront_ORM_practise/store/models.py
+++ CWM_Django_part1/storefront_ORM_practise/store/models.py
@@ -12,7 +12,10 @@ class Collection(models.Model):
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
     
     def __str__(self) -> str:
-        return self.title
+        return str(self.title)
+    
+    class Meta:
+        ordering = ['title']
 
 
 class Product(models.Model):
@@ -24,6 +27,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+    
+    def __str__(self) -> str:
+        return str(self.title)
+    
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
@@ -43,6 +52,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 
 class Order(models.Model):
