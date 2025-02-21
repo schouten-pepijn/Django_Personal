56c6959c72cc735e24b34dfdc2fa7d343a26f1ba
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-21 17:57:32 +0100

    add searching and sorting

--- CWM_Django_part2/storefront2_class_views/store/views.py
+++ CWM_Django_part2/storefront2_class_views/store/views.py
@@ -4,6 +4,7 @@ from django.db.models import Count
 
 from django_filters.rest_framework import DjangoFilterBackend
 
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
@@ -11,6 +12,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .filters import ProductFilter
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
@@ -28,8 +30,15 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['collection_id', 'unit_price']
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
+    ]
+    # filterset_fields = ['collection_id', 'unit_price']
+    filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
