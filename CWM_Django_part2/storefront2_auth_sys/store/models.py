d5cd74fbef6eb8f6141099c8a6741a6f8fb6498f
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-23 12:13:47 +0100

    .

--- CWM_Django_part2/storefront2_auth_sys/store/models.py
+++ CWM_Django_part2/storefront2_auth_sys/store/models.py
@@ -81,6 +81,11 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can cancel order')
+        ]
 
 
 class OrderItem(models.Model):
