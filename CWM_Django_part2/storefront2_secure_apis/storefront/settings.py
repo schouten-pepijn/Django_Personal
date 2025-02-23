87f31e92a765ea7dea876a5092c9579a2ec19657
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-23 21:48:09 +0100

    add user credentials post request

--- CWM_Django_part2/storefront2_secure_apis/storefront/settings.py
+++ CWM_Django_part2/storefront2_secure_apis/storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,6 +153,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 AUTH_USER_MODEL = 'core.User'
