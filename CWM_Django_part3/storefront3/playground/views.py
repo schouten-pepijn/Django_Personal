ca35054cf84453c422f71e8b6c9d09a29f15449c
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-25 23:34:30 +0100

    added templated email

--- CWM_Django_part3/storefront3/playground/views.py
+++ CWM_Django_part3/storefront3/playground/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
-
+from templated_mail.mail import BaseEmailMessage
 def say_hello(request):
     try:
         send_mail('subject', 'message', 'tjY4o@example.com', ['tjY4o@example.com'])
@@ -11,6 +11,13 @@ def say_hello(request):
         email = EmailMessage('subject', 'message', 'tjY4o@example.com', ['tjY4o@example.com'])
         email.attach_file('playground/media/princess.png')
         email.send()
+        
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context = {'name': 'Pepijn'}
+        )
+        message.send(['tjY4o@example.com'])
+        
     except BadHeaderError:
         return HttpResponse('Invalid header found.')
 
