12d3c21be3810090198b0a020940d1e6ce611705
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-25 23:25:12 +0100

    attach file to email

--- CWM_Django_part3/storefront3/playground/views.py
+++ CWM_Django_part3/storefront3/playground/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 
 def say_hello(request):
     try:
         send_mail('subject', 'message', 'tjY4o@example.com', ['tjY4o@example.com'])
+        
         mail_admins('subject', 'message', html_message='message')
+        
+        email = EmailMessage('subject', 'message', 'tjY4o@example.com', ['tjY4o@example.com'])
+        email.attach_file('playground/media/princess.png')
+        email.send()
     except BadHeaderError:
         return HttpResponse('Invalid header found.')
 
