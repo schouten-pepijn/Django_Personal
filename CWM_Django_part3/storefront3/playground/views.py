6770710e70a5755f38f5aa77e80efc12aba6e5a4
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-26 00:13:11 +0100

    .

--- CWM_Django_part3/storefront3/playground/views.py
+++ CWM_Django_part3/storefront3/playground/views.py
@@ -7,19 +7,19 @@ from .tasks.tasks import notify_customer
 def say_hello(request):
     try:
         send_mail('subject', 'message', 'tjY4o@example.com', ['tjY4o@example.com'])
-        
+
         mail_admins('subject', 'message', html_message='message')
-        
+
         email = EmailMessage('subject', 'message', 'tjY4o@example.com', ['tjY4o@example.com'])
         email.attach_file('playground/media/princess.png')
         email.send()
-        
+
         message = BaseEmailMessage(
             template_name='emails/hello.html',
             context = {'name': 'Pepijn'}
         )
         message.send(['tjY4o@example.com'])
-        
+      
     except BadHeaderError:
         return HttpResponse('Invalid header found.')
 
