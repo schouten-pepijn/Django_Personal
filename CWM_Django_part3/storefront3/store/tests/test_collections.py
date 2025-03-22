ecb71b716335b04afffc5ef1fd6353cd8760688f
Pepijn Schouten <pepijn.sch@gmail.com>
2025-03-22 22:17:46 +0100

    Create pytest.ini

--- CWM_Django_part3/storefront3/store/tests/test_collections.py
+++ CWM_Django_part3/storefront3/store/tests/test_collections.py
@@ -1,7 +1,9 @@
 from rest_framework.test import APITClient
 from rest_framework import status
+import pytest
 
 
+@pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_returns_401(self):
         # Arange
